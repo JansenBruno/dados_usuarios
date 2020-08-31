98e2c637d9ec0c4866b51ad9d0e86acd7253227e
Jansen Bruno <jansenbruno74@gmail.com>
2020-08-31 01:13:19 -0300

    C0001 random users

---
+++ index.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+fs.writeFile("usuarios.csv", `Primeiro_nome,Sobrenome,Email,Idade,Genero,Username,Password\n`,
+
+    (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('Arquivo criado');
+    })
+
+async function generate() {
+
+    let resp = await fetch("http://randomuser.me/api/?results=500");
+    if (resp.ok) {
+
+        const usuarios = await resp.json();
+        console.log(usuarios.results);
+
+        usuarios.results.forEach(user =>
+            fs.createWriteStream('usuarios.csv', { flags: "a" }).write(
+                               `${user.name.first},
+                                 ${user.name.last},
+                                 ${user.email},
+                                 ${user.dob.age},
+                                 ${user.gender},
+                                 ${user.login.username},
+                                 ${user.login.password}`,
+                (er) => {
+                    if (er) throw err;
+                    console.log('OK');
+                }
+            ));
+    }
+}
+generate();
